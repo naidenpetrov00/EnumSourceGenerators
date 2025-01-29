e4b4b2e4a2035c83f262b95f092055aa449d8a25
naiden00 <naiden.petrov.31.12.00@gmail.com>
2025-01-30 00:06:40 +0200

    Add a predicate and transform for the nodes that pass the predicate

--- EnumSourceGenerators/EnumGenerator.cs
+++ EnumSourceGenerators/EnumGenerator.cs
@@ -1,4 +1,6 @@
+using EnumSourceGenerators;
 using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Text;
 
 [Generator]
@@ -6,11 +8,39 @@ class EnumGenerator : IIncrementalGenerator
 {
     public void Initialize(IncrementalGeneratorInitializationContext context)
     {
+        //adds source code of the attribute to the compilation
         context.RegisterPostInitializationOutput(ctx =>
             ctx.AddSource(
                 "EnumExtensionsAttribute.g.cs",
                 SourceText.From(SourceGenerationHelper.Attribute)
             )
         );
+
+        IncrementalValuesProvider<EnumToGenerate?> enumsToGenerate = context
+            .SyntaxProvider.CreateSyntaxProvider(
+                // predicate runs on every keystroke
+                //thats why we check if its only attribute but not if its [EnumExtensions] here!
+                predicate: static (s, _) => IsSyntaxTargetForGeneration(s),
+                //ctx: syntax notes that passed the predicate
+                transform: static (ctx, _) => GetSemanticTargetForGeneration(ctx)
+            )
+            //filters out if transform have not found enum with attribute and is returning null
+            .Where(static m => m is not null);
+    }
+
+    //check idf node contains Attributes
+    private static bool IsSyntaxTargetForGeneration(SyntaxNode node)
+    {
+        if (node is EnumDeclarationSyntax m && m.AttributeLists.Count > 0)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    static EnumToGenerate? GetSemanticTargetForGeneration(GeneratorSyntaxContext context)
+    {
+        //Check if enums contains [EnumExtensions]
+        return null;
     }
 }
--- EnumSourceGenerators/Infrastructure/Attributes/SourceGenerationHelper.cs
+++ EnumSourceGenerators/Infrastructure/Attributes/SourceGenerationHelper.cs
@@ -2,7 +2,7 @@ public static class SourceGenerationHelper
 {
     public const string Attribute =
         @"
-namespace NetEscapades.EnumGenerators
+namespace EnumSourceGenerators
 {
     [System.AttributeUsage(System.AttributeTargets.Enum)]
     public class EnumExtensionsAttribute : System.Attribute
